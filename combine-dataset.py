682955db8be52598e473bc7cd95b9684d3f89ce7
Anuj Pahuja <apnoob.13@gmail.com>
2017-08-09 16:53:00 +0530

    Enable taking directory list via single file.

--- combine-dataset.py
+++ combine-dataset.py
@@ -19,7 +19,7 @@ def compare(a, b):
 parser = argparse.ArgumentParser(
     description='Combine image datasets from different directories')
 parser.add_argument("--input_dirs", required=True,
-                    help="1 or more input directories", nargs="+")
+                    help="1 or more input directories or a file containing list of directories", nargs="+")
 parser.add_argument("--output_dir", required=True, help="Output directory")
 parser.add_argument("--extension", required=True, type=str,
                     help="File extension (png, jpg etc.)")
@@ -38,9 +38,19 @@ else:
     img_list = glob.glob(regex)
     count = len(img_list) + 1
 
+if len(input_dirs) == 1:
+    if not os.path.isdir(input_dirs[0]):
+        with open(input_dirs[0]) as f:
+            content = f.readlines()
+        content = [x.strip() for x in content]
+        input_dirs = content
+
 for dir_name in input_dirs:
     if not os.path.isdir(dir_name):
         continue
+
+    if dir_name[-1] != '/':
+        dir_name = dir_name + '/'
 
     img_list = []
     regex = dir_name + '*' + img_type
@@ -52,6 +62,7 @@ for dir_name in input_dirs:
         file_name_list.append(file_name)
 
     file_name_list.sort(compare)
+    print "Copying files from %s" % dir_name
     for val in file_name_list:
         count_str = "%06d" % count
         src_img_name = dir_name + val
